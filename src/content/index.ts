786ad4b0738396ce62ebb40d37055fd2d0b5abe4
daPots <srivaths.ravva@gmail.com>
2025-02-22 18:05:51 -0500

    Chrome extension basic setup

---
+++ src/content/index.ts
@@ -0,0 +1,8 @@
+chrome.runtime.onMessage.addListener(
+    function(msg, sender, sendResponse) {
+      const dogImg: HTMLImageElement = document.createElement('img');
+      dogImg.src = msg;
+      document.body.appendChild(dogImg);
+    }
+);
+
